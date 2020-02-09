01c5880a0e003a289ca84e313310d7368baa253a
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-09 13:59:22 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,37 @@
 import React from 'react';
- 
+
 class Form extends React.Component {
   state = {
     firstName: "John",
     lastName: "Henry",
     submittedData: []
   }
- 
+
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
     })
   }
- 
+
   handleLastNameChange = event => {
     this.setState({
       lastName: event.target.value
     })
   }
- 
+
   handleSubmit = event => {
     event.preventDefault()
     let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
     let dataArray = this.state.submittedData.concat(formData)
     this.setState({submittedData: dataArray})
   }
- 
+
   listOfSubmissions = () => {
     return this.state.submittedData.map(data => {
       return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
     })
   }
- 
+
   render() {
     return (
       <div>
@@ -53,5 +53,5 @@ class Form extends React.Component {
     )
   }
 }
- 
+
 export default Form;
